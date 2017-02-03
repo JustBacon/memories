4a89bbfebafb41f5d71082ac747d3bfe615cab9e
Christian Buco <christianbuco17@gmail.com>
2017-02-03 09:15:36 -1000

    updated 2/3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 console.log("hi")
+var clicked = 0;
+var maxClick = 3;
 
-function flip(id){
-	document.getElementById(id).className = "boxAnimate"
+/* document.getElementById(id).onclick = */function flip(id){
+	clicked ++;
+	document.getElementById(id).style.transform = "rotateY(180deg)";
+	if (clicked > maxClick) {
+		document.getElementById(id).disabled = true;
+	}
+if (document.getElementById(id).style.transform === "rotateY(180deg)"){
+		setTimeout(function(){document.getElementById(id).style.transform = "rotateY(0deg)";
+	},1000);
+}
 }
